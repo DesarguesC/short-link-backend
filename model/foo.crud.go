0cc049f6c03c83953da56a79c5ea6e2d41a371d9
stein_wang <stein_wang@qq.com>
2022-12-09 01:04:09 +0800

    commit qwq

--- model/foo.crud.go
+++
@@ -1,3 +0,0 @@
-package model
-
-// TODO: add crud interface here
--- model/model.go
+++ model/model.go
@@ -1,3 +1,13 @@
 package model
 
 // TODO: add new model
+type Url struct {
+	Id      int    `gorm:"type:uint;primaryKey autoincrement" form:"id" json:"id"`
+	UserId  int    `gorm:"type:varchar(10) column:user_id" form:"user_id" json:"user_id"`
+	Origin  string `gorm:"type:varchar(200)" form:"origin" json:"origin"`
+	Short   string `gorm:"type:varchar(40)" form:"short" json:"short"`
+	Content string `gorm:"type:varchar(100)" form:"content" json:"content"`
+	Owner   string `gorm:"type:varchar(100)" form:"owner" json:"owner"`
+	//StartTime  time.Time `gorm:"type:datetime;autoCreateTime"`
+	//ExpireTime time.Time `gorm:"type:datetime"`
+}
