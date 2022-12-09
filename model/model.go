ce7de2c9ca402b25a58efd9a64792a63248b0597
Chandy0527 <425593160@qq.com>
2022-12-09 10:45:17 +0800

    fix

--- model/model.go
+++ model/model.go
@@ -1,7 +1,9 @@
 package model
+
 import "time"
+
 // TODO: add new model
-type Url struct{
+type Url struct {
 	Id         int       `gorm:"type:uint;primaryKey autoincrement" form:"id" json:"id"`
 	UserId     int       `gorm:"type:varchar(10) column:user_id" form:"user_id" json:"user_id"`
 	Origin     string    `gorm:"type:varchar(200)" form:"origin" json:"origin"`
@@ -9,4 +11,12 @@ type Url struct{
 	Comment    string    `gorm:"type:varchar(100)" form:"comment" json:"comment"`
 	StartTime  time.Time `gorm:"type:datetime;autoCreateTime"`
 	ExpireTime time.Time `gorm:"type:datetime"`
+}
+
+type Users struct {
+	Name  string `gorm:"type:varchar(20)"`
+	Email string `gorm:"type:varchar(50)"`
+	Pwd   string `gorm:"type:varchar(90)"`
+	SecQ  string `gorm:"type:varchar(100)"`
+	SecA  string `gorm:"type:varchar(100)"`
 }
