ae6552823e57c2c2d00b63b84ec46cd82b77c1c9
stein_wang <stein_wang@qq.com>
2022-12-09 20:43:24 +0800

    Update response.go

--- app/response/response.go
+++ app/response/response.go
@@ -1,8 +1,16 @@
+package response
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
 type Response struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
 	Data interface{} `json:"data"`
 }
+
 //demo
 func SendResponse(c echo.Context, code int, msg string, data ...interface{}) error {
 	return c.JSON(http.StatusOK, Response{
